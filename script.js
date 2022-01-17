fec3c50f75d55a4bf492896bff7fe2055d580ee1
SLOTH Jean-Michel <jeanmichelsloth@gmail.com>
2022-01-17 20:29:06 +0100

    Update let to const

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 window.setInterval(() => {
-    let timeToDisplay = createTimeFromDate(new Date());
+    const timeToDisplay = createTimeFromDate(new Date());
     displayTime(timeToDisplay);
 }, 1000);
 
 function createTimeFromDate(dateInput) {
-    let formattedHours = formatTimeItem(dateInput.getHours());
-    let formattedMinutes = formatTimeItem(dateInput.getMinutes());
-    let formattedSeconds = formatTimeItem(dateInput.getSeconds());
+    const formattedHours = formatTimeItem(dateInput.getHours());
+    const formattedMinutes = formatTimeItem(dateInput.getMinutes());
+    const formattedSeconds = formatTimeItem(dateInput.getSeconds());
     return formattedHours + ':' + formattedMinutes + ':' + formattedSeconds;
 }
 
